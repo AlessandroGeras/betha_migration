97d2b4ee33cb31a4258d41c9476b476c109775ed
Alessandro Geras <alessandrogeras@gmail.com>
2023-12-25 13:34:10 -0300

    Outsourced

---
+++ components/outsourced.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Outsourced = () => {
+
+  return (
+    <div className="flex-1" id="Dashboard">
+      {/* Barra Superior Azul Fixada no Topo */}
+      <div className="bg-blue-500 text-white p-2 text-left mb-4 w-full">
+        {/* Conteúdo da Barra Superior, se necessário */}
+        <span className='ml-2'>Terceiros</span>
+      </div>
+
+      <div className="flex items-center justify-center h-5/6 pt-28 px-20">
+        <div className="container mx-auto">
+          {/* Área de Documentos a Vencer */}
+          <div className='items-center '>
+            <div className="flex justify-center mt-[-40px] mb-24 px-26">
+              {/* Três Divs Centralizadas Lado a Lado */}
+              <div className="mx-2 my-2 flex-1 bg-white px-3 py-3 rounded shadow text-center">
+                <div className="mt-[-15px]">Quantidade de documentos ativos</div>
+                <div className="mt-2 text-4xl text-gray-600"></div>
+              </div>
+              <div className="mx-2 my-2 flex-1 bg-white px-3 py-3 rounded shadow text-center">
+                <div className="mt-[-15px]">Quantidade de terceiros ativos</div>
+                <div className="mt-2 text-4xl text-gray-600"></div>
+              </div>
+              <div className="mx-2 my-2 flex-1 bg-white px-3 py-3 rounded shadow text-center">
+                <div className="mt-[-15px]">Quantidade de colaboradores ativos</div>
+                <div className="mt-2 text-4xl text-gray-600"></div>
+              </div>
+            </div>
+
+            <div className="flex justify-center px-32 relative top-[-30px]">
+              {/* Duas Divs Centralizadas Lado a Lado */}
+              <div className="mx-2 my-2 flex-1 bg-white px-14 py-3 mx-4 rounded shadow text-center">
+                <div className="mt-[-15px] text-2xl text-gray-600">Documentos a vencer</div>
+                <div className="mt-2 text-gray-600 text-sm">Documentos com 30 dias ou menos da data de vencimento</div>
+                <div className='mt-2 flex justify-center bg-yellow-500 mx-auto w-[115px]'>
+                  <div className="text-gray-600 text-5xl text-white py-2"></div>
+                </div>
+                <div className='text-xs mt-2 text-gray-600'>Clique no número para listar os documentos</div>
+              </div>
+              <div className="mx-2 my-2 flex-1 bg-white px-14 py-3 mx-4 rounded shadow text-center">
+                <div className="mt-[-15px] text-2xl text-gray-600">Documentos vencidos</div>
+                <div className="mt-2 text-gray-600 text-sm">Documentos com data inferior ao dia atual</div>
+                <div className="mt-2 flex justify-center mx-auto w-[115px]">
+                  <div className="text-gray-600 text-5xl text-white py-2"></div>
+                </div>
+                <div className='text-xs mt-2 text-gray-600'>Clique no número para listar os documentos</div>
+              </div>            
+            </div>
+            <div className="flex justify-center px-32 relative top-[-10px]">
+              {/* Duas Divs Centralizadas Lado a Lado */}              
+              <div className="mx-2 my-2 flex-1 bg-white px-14 py-3 mx-4 rounded shadow text-center">
+                <div className="mt-[-15px] text-2xl text-gray-600">Documentos faltantes</div>
+                <div className="mt-2 text-gray-600 text-sm">Documentos solicitados e não enviados</div>
+                <div className="mt-2 flex justify-center mx-auto w-[115px] bg-yellow-500">
+                  <div className="text-gray-600 text-5xl text-white py-2"></div>
+                </div>
+                <div className='text-xs mt-2 text-gray-600'>Clique no número para listar os documentos</div>
+              </div>
+              <div className="mx-2 my-2 flex-1 bg-white px-14 py-3 mx-4 rounded shadow text-center">
+                <div className="mt-[-15px] text-2xl text-gray-600">Documentos à analisar</div>
+                <div className="mt-2 text-gray-600 text-sm">Documentos pendentes de análise</div>
+                <div className="mt-2 flex justify-center mx-auto w-[115px] bg-yellow-500">
+                  <div className="text-gray-600 text-5xl text-white py-2"></div>
+                </div>
+                <div className='text-xs mt-2 text-gray-600'>Clique no número para listar os documentos</div>
+              </div>            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Outsourced ;
