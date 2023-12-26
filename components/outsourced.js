4e11fe67a39fc346d2390195bf08f973d403cdc0
Alessandro Geras <alessandrogeras@gmail.com>
2023-12-26 10:33:12 -0300

    Outsourced Pagination Tests

--- components/outsourced.js
+++ components/outsourced.js
@@ -7,16 +7,16 @@ const Outsourced = ({ finishedLoading }) => {
     docs: { rows: [], count: 0, outsourcedCount: 0 },
   });
   const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 10;
+  const [pageSize, setPageSize] = useState(10);
   const [sortOrder, setSortOrder] = useState('asc');
   const [sortColumn, setSortColumn] = useState('NM_USUARIO');
 
   const columnWidths = {
     'NM_USUARIO': '350px',
     'CNPJ': '200px',
-    'ENDEREÇO': '350px',
-    'CIDADE': '350px',
-    'UF': '50px',
+    'ENDEREÇO': '355px',
+    'CIDADE': '320px',
+    'UF': '60px',
     'TELEFONE': '150px',
   };
 
@@ -29,14 +29,19 @@ const Outsourced = ({ finishedLoading }) => {
     });
   };
 
- const handleSort = (columnName) => {
-  if (columnName === sortColumn) {
-    setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
-  } else {
-    setSortColumn(columnName);
-    setSortOrder('asc');
-  }
-};
+  const handleSort = (columnName) => {
+    if (columnName === sortColumn) {
+      setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
+    } else {
+      setSortColumn(columnName);
+      setSortOrder('asc');
+    }
+  };
+
+  const handlePageSizeChange = (size) => {
+    setPageSize(size);
+    setCurrentPage(1);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -60,7 +65,7 @@ const Outsourced = ({ finishedLoading }) => {
       }
     };
     fetchData();
-  }, [currentPage, sortColumn, sortOrder]);
+  }, [currentPage, pageSize, sortColumn, sortOrder]);
 
   const totalPages = Math.ceil((documents.docs && documents.docs.count) / pageSize) || 1;
 
@@ -85,35 +90,65 @@ const Outsourced = ({ finishedLoading }) => {
               <div
                 key={column}
                 className={`header-cell border border-gray-200 py-1 pl-1 cursor-pointer flex`}
-                style={{ width: columnWidths[column] }}
-                onClick={() => handleSort(column)}>
+                style={{ width: column === 'CIDADE' ? (pageSize === 10 ? '340px' : '320px') : columnWidths[column] }}
+                onClick={() => handleSort(column)}
+              >
                 {column}
                 <div className='ml-auto flex'>
-                {sortColumn === column && (
-                  sortOrder === 'asc' ? <span className="text-xl mt-[-3px]">↑</span> : <span className="text-xl mt-[-3px]">↓</span>
-                )}
-                <PiFunnelLight className='text-xl mt-0.5'/>
+                  {sortColumn === column && (
+                    sortOrder === 'asc' ? <span className="text-xl mt-[-3px]">↑</span> : <span className="text-xl mt-[-3px]">↓</span>
+                  )}
+                  <PiFunnelLight className='text-xl mt-0.5'/>
                 </div>
               </div>
             ))}
           </div>
 
-          {documents.docs.rows.map((document) => (
-            <div className='flex text-gray-700' key={document.id || Math.random().toString()}>
+          {documents.docs.rows.map((document, index) => (
+            <div
+              className={`flex text-gray-700 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
+              key={document.id || Math.random().toString()}
+            >
               {Object.keys(columnWidths).map((column) => (
-                <div key={column} className={`column-cell border border-gray-200 py-2 pl-1`} style={{ width: columnWidths[column] }}>
+                <div
+                  key={column}
+                  className={`column-cell border border-gray-200 py-2 pl-1`}
+                  style={{ width: column === 'CIDADE' ? (pageSize === 10 ? '340px' : '320px') : columnWidths[column] }}
+                >
                   {document[column]}
                 </div>
               ))}
             </div>
           ))}
 
-          <div className="flex mt-4 justify-between border-t border-gray-200 pt-4">
-            <button onClick={goToPreviousPage} disabled={currentPage === 1} className="border border-gray-200 px-4 py-2 rounded">
+          <div className="flex mt-4 justify-between border-t border-gray-200 items-center">
+            <button
+              onClick={goToPreviousPage}
+              disabled={currentPage === 1}
+              className={`border border-gray-200 px-4 py-2 rounded bg-blue-500 text-white ${currentPage === 1 ? 'invisible' : ''}`}
+            >
               Página Anterior
-            </button>
-            <span>Página {currentPage}</span>
-            <button onClick={goToNextPage} disabled={currentPage === totalPages} className="border border-gray-200 px-4 py-2 rounded">
+            </button>            
+            <div className="flex items-center">
+              <span className="mr-2">Registros por página:</span>
+              {[10, 25, 50, 100].map((size) => (
+                <button
+                  key={size}
+                  className={`px-2 py-1 border ${
+                    size === pageSize ? 'bg-blue-500 text-white' : ''
+                  }`}
+                  onClick={() => handlePageSizeChange(size)}
+                >
+                  {size}
+                </button>
+              ))}
+            </div>
+            <span className=''>Página {currentPage}</span>
+            <button
+              onClick={goToNextPage}
+              disabled={currentPage === totalPages}
+              className={`border border-gray-200 px-4 py-2 rounded bg-blue-500 text-white ${currentPage === totalPages ? 'invisible' : ''}`}
+            >
               Próxima Página
             </button>
           </div>
