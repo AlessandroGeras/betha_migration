439526f7a0c30f5b4d02159ffc110939017b550d
Alessandro Geras <alessandrogeras@gmail.com>
2024-02-01 14:03:25 -0300

    Approve Docs

--- pages/api/teste.js
+++ pages/api/teste.js
@@ -1,28 +1,25 @@
-import users from '../../models/users';
-import outsourceds from '../../models/outsourceds';
-import Sequelize from 'sequelize-oracle';
 import Oracledb from 'oracledb';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';  // Importe o módulo JWT
-
 
 dotenv.config();
-let report;
-
-try {
-    Oracledb.initOracleClient({
-      libDir: '../../../opt/oracle/instantclient_19_16',
-    });
-   report = 'Cliente Oracle inicializado com sucesso.';
-  } catch (error) {
-    report = error;
-  }
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
-    
+
+    let report;
+
+    try {
+      Oracledb.initOracleClient({
+        libDir: '../../../opt/oracle/instantclient_19_16',
+      });
+      report = 'Cliente Oracle inicializado com sucesso.';
+      console.log(report); // Exibe a mensagem de sucesso no console
+    } catch (error) {
+      report = error;
+      console.error('Erro ao inicializar o cliente Oracle:', error); // Exibe o erro no console
+    }
+
     res.status(200).json({ report });
   }
 }
