7f35d5cfcce646cd5de66855e265f5e8c063448c
Alessandro Geras <alessandrogeras@gmail.com>
2024-02-09 13:33:04 -0300

    Produção

--- pages/api/store-outsourced.js
+++ pages/api/store-outsourced.js
@@ -25,9 +25,9 @@ export default async function handler(req, res) {
             id_usuario
         } = req.body;
 
-        
+
         try {
-            jwt.verify(token, process.env.SECRET);    
+            jwt.verify(token, process.env.SECRET);
 
             // Criação da empresa
             const storeEmpresa = await outsourceds.create({
@@ -42,34 +42,42 @@ export default async function handler(req, res) {
                 UF: uf,
                 TELEFONE: telefone,
                 ID_USUARIO_INTERNO: "N",
-                COLABORADOR_TERCEIRO: "N",                
+                COLABORADOR_TERCEIRO: "N",
                 ID_USUARIO: id_usuario,
                 CATEGORIA_PRINCIPAL: categorias.join(', '), // Concatenando os valores da array
             });
 
             // Criação dos documentos apenas se a empresa for criada com sucesso
             if (storeEmpresa) {
-                for (const categoria of categorias) { // Iterando sobre as categorias
-                    const storeDocumentos = await documents.create({
-                        STATUS: "Pendente",
-                        TIPO_DOCUMENTO: categoria,
-                        TERCEIRO: nomeTerceiro,
+                for (const categoria of categorias) {
+                    const category = await categoria_terceiros.findOne({
+                        where: {
+                            CATEGORIA: categoria
+                        },
                     });
-                }
 
-                res.status(200).json({ success: true, message: 'Terceiro criado.' });
+                    if (category) {
+                        const tipoDocumentos = category.TIPO_DOCUMENTO.split(', ');
+
+                        for (const tipoDocumento of tipoDocumentos) {
+                            const storeDocumentos = await documents.create({
+                                STATUS: "Pendente",
+                                TIPO_DOCUMENTO: tipoDocumento,
+                                TERCEIRO: nomeTerceiro,
+                            });
+                        }
+                    } else {
+                        console.log('Falha ao criar usuário Terceiro.');
+                        res.status(400).json({ success: false, message: 'Falha ao criar usuário Terceiro.' });
+                    }
+                } catch (error) {
+                    console.error('Erro ao salvar o usuário Terceiro:', error);
+                    res.status(500).json({ success: false, message: 'Erro ao salvar o usuário Terceiro.' });
+                } finally {
+                    // Certifique-se de fechar a conexão no bloco finally para garantir que a conexão seja fechada, independentemente do resultado
+
+                }
             } else {
-                console.log('Falha ao criar usuário Terceiro.');
-                res.status(400).json({ success: false, message: 'Falha ao criar usuário Terceiro.' });
+                res.status(405).json({ success: false, message: 'Método não permitido.' });
             }
-        } catch (error) {
-            console.error('Erro ao salvar o usuário Terceiro:', error);
-            res.status(500).json({ success: false, message: 'Erro ao salvar o usuário Terceiro.' });
-        } finally {
-            // Certifique-se de fechar a conexão no bloco finally para garantir que a conexão seja fechada, independentemente do resultado
-            
         }
-    } else {
-        res.status(405).json({ success: false, message: 'Método não permitido.' });
-    }
-}
