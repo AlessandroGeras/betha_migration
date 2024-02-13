5650029dfb409ecebd34f8a4f90a7918254fb723
Alessandro Geras <alessandrogeras@gmail.com>
2024-02-13 15:37:32 -0300

    Análise

--- pages/find-document.tsx
+++ pages/find-document.tsx
@@ -779,7 +779,7 @@ const FindDocument = () => {
 
                             {reproveAnalysis && (
                                 <div className='flex'>
-                                    {showReproveButton && (<button className="mx-auto mt-4 w-[300px]" onClick={() => handleSubmitSendAnalysis("Reprovado")}>
+                                    {showReproveButton==true && (<button className="mx-auto mt-4 w-[300px]" onClick={() => handleSubmitSendAnalysis("Reprovado")}>
                                         <span className="bg-blue-950 text-white py-[9.5px] shadow-md w-[300px] p-2 rounded-md block text-center">
                                             Reprovar o documento
                                         </span>
