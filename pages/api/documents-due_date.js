c23896b02762172ca73518ac8e0e0347fec5c7ff
Alessandro Geras <alessandrogeras@gmail.com>
2024-02-14 06:26:56 -0300

    Collaborators

--- pages/api/documents-due_date.js
+++ pages/api/documents-due_date.js
@@ -1,18 +1,22 @@
 import documents from '../../models/documents';
-import Sequelize from 'sequelize-oracle';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import outsourceds from '../../models/outsourceds';
+import Sequelize from 'sequelize-oracle';
 
 dotenv.config();
 
-const getAllDocs = async (pageSize) => {
+const getAllDocs = async (pageSize, user) => {
   let allDocs = [];
   let offset = 0;
 
   while (true) {
     try {
       const result = await documents.findAll({
-        where: Sequelize.literal("TRUNC(VENCIMENTO) < TRUNC(SYSDATE)"),
+        where: {
+          VENCIMENTO: Sequelize.literal("TRUNC(VENCIMENTO) < TRUNC(SYSDATE)"),
+          ...(user && { TERCEIRO: user.NOME_TERCEIRO }) // Adiciona filtro por NOME_TERCEIRO se user não for null ou undefined
+        },
         offset,
         limit: pageSize,
       });
@@ -34,61 +38,122 @@ const getAllDocs = async (pageSize) => {
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { token, getAll } = req.body;
+    const { token, getAll, id, role } = req.body;
 
     if (!token) {
       return res.redirect(302, '/login');
     }
 
     try {
-      jwt.verify(token, process.env.SECRET);      
-
-      const outsourcedCount = await documents.count();
+      jwt.verify(token, process.env.SECRET);
 
       const pageSize = parseInt(req.query.pageSize) || 10;
       const page = parseInt(req.query.page) || 1;
 
-      if (getAll) {
-        try {
-          const allDocs = await getAllDocs(pageSize);    
+      if (role === "external" && id) {
+        // Verificar o usuário pelo ID
+        const user = await outsourceds.findOne({
+          where: {
+            ID_USUARIO: id
+          }
+        });
 
-          res.status(200).json({
-            success: true,
-            message: 'Documentos encontrados',
-            docs: {
-              rows: allDocs,
-              count: allDocs.length,
-              outsourcedCount: allDocs.length,
-            },
-          });
-        } catch (error) {
-          console.error('Error fetching all documents:', error);
-          res.status(500).json({ success: false, message: 'Erro ao obter todos os documentos' });
+        if (!user) {
+          return res.status(404).json({ success: false, message: 'Usuário não encontrado' });
         }
-      } else {
-        try {
-          const docs = await documents.findAndCountAll({
-            where: Sequelize.literal("TRUNC(VENCIMENTO) < TRUNC(SYSDATE)"),
-            offset: (page - 1) * pageSize,
-            limit: pageSize,
-          });
 
-          if (docs) {
+        if (getAll) {
+          try {
+            const allDocs = await getAllDocs(pageSize, user);
+
             res.status(200).json({
               success: true,
               message: 'Documentos encontrados',
               docs: {
-                rows: docs.rows,
-                count: docs.count,
-                outsourcedCount: docs.count,
+                rows: allDocs,
+                count: allDocs.length,
+                outsourcedCount: allDocs.length,
               },
             });
-          } else {
-            res.status(400).json({ success: false, message: 'Não foi possível obter os documentos.' });
+          } catch (error) {
+            console.error('Error fetching all documents:', error);
+            res.status(500).json({ success: false, message: 'Erro ao obter todos os documentos' });
           }
-        } catch (error) {
-          console.error('Error fetching paginated documents:', error);
-          res.status(500).json({ success: false, message: 'Erro ao obter os documentos paginados' });
+        } else {
+          try {
+            const docs = await documents.findAndCountAll({
+              where: {
+                VENCIMENTO: Sequelize.literal("TRUNC(VENCIMENTO) < TRUNC(SYSDATE)"),
+                TERCEIRO: user.NOME_TERCEIRO // Adiciona filtro por NOME_TERCEIRO
+              },
+              offset: (page - 1) * pageSize,
+              limit: pageSize,
+            });
+
+            if (docs) {
+              res.status(200).json({
+                success: true,
+                message: 'Documentos encontrados',
+                docs: {
+                  rows: docs.rows,
+                  count: docs.count,
+                  outsourcedCount: docs.count,
+                },
+              });
+            } else {
+              res.status(400).json({ success: false, message: 'Não foi possível obter os documentos.' });
+            }
+          } catch (error) {
+            console.error('Error fetching paginated documents:', error);
+            res.status(500).json({ success: false, message: 'Erro ao obter os documentos paginados' });
+          }
+        }
+      } else {
+        // Manter o código original
+        if (getAll) {
+          try {
+            const allDocs = await getAllDocs(pageSize, null);
+
+            res.status(200).json({
+              success: true,
+              message: 'Documentos encontrados',
+              docs: {
+                rows: allDocs,
+                count: allDocs.length,
+                outsourcedCount: allDocs.length,
+              },
+            });
+          } catch (error) {
+            console.error('Error fetching all documents:', error);
+            res.status(500).json({ success: false, message: 'Erro ao obter todos os documentos' });
+          }
+        } else {
+          try {
+            const docs = await documents.findAndCountAll({
+              where: {
+                VENCIMENTO: Sequelize.literal("TRUNC(VENCIMENTO) < TRUNC(SYSDATE)")
+              },
+              offset: (page - 1) * pageSize,
+              limit: pageSize,
+            });
+
+            if (docs) {
+              res.status(200).json({
+                success: true,
+                message: 'Documentos encontrados',
+                docs: {
+                  rows: docs.rows,
+                  count: docs.count,
+                  outsourcedCount: docs.count,
+                },
+              });
+            } else {
+              res.status(400).json({ success: false, message: 'Não foi possível obter os documentos.' });
+            }
+          } catch (error) {
+            console.error('Error fetching paginated documents:', error);
+            res.status(500).json({ success: false, message: 'Erro ao obter os documentos paginados' });
+          }
         }
       }
     } catch (error) {
@@ -100,7 +165,7 @@ export default async function handler(req, res) {
         res.status(500).json({ success: false, message: 'Erro ao contatar o servidor' });
       }
     } finally {
-      
+
     }
   }
 }
