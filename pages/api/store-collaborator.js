25dec50a0a3e069f2626fb16d9066490e7692723
Alessandro Geras <alessandrogeras@gmail.com>
2024-02-16 17:32:33 -0300

    Collaborators

--- pages/api/store-collaborator.js
+++ pages/api/store-collaborator.js
@@ -65,6 +65,7 @@ export default async function handler(req, res) {
 
             // Criação dos documentos apenas se a empresa for criada com sucesso
             if (Store) {
+                console.log("Gravação feito");
                 for (const categoria of categorias) {
                     const category = await categoria_colaboradores.findOne({
                         where: {
