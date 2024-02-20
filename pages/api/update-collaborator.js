829f1f424d6324878958e9a15c5458d9c273502c
AlessandroGeras <alessandrogeras@gmail.com>
2024-02-20 10:31:04 -0300

    Collaborators

--- pages/api/update-collaborator.js
+++ pages/api/update-collaborator.js
@@ -39,6 +39,7 @@ export default async function handler(req, res) {
            /*  existingUser.CIDADE = cidade; */
             /* existingUser.UF = uf; */
             /* existingUser.TELEFONE = telefone; */
+            existingUser.OBS_STATUS = obs_status;
             existingUser.CPF = cpf;
             existingUser.NM_USUARIO = usuario;
             existingUser.FUNCAO = principal;
