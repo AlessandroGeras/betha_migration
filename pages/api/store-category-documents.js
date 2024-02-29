a9a2a9fd7117e2884f58f729e0fed4d2af6aa7d9
AlessandroGeras <alessandrogeras@gmail.com>
2024-02-29 14:03:21 -0300

    Auditoria

--- pages/api/store-category-documents.js
+++ pages/api/store-category-documents.js
@@ -6,50 +6,54 @@ import jwt from 'jsonwebtoken';
 dotenv.config();
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      const { categoria, numeracao, formato_vencimento, auditoria,campos_vencimento, token } = req.body;
-  
-      if (!token) {
-        return res.redirect(302, '/login');
-      }
-  
-      try {
-        jwt.verify(token, process.env.SECRET); 
-  
-        const store = await categoria_documentos.create({
-          CATEGORIA: categoria,
-          NUMERACAO: numeracao,
-          FORMATO_VENCIMENTO: formato_vencimento,
-          AUDITORIA: auditoria,
-          CAMPOS_VENCIMENTO: campos_vencimento,
-        }, {
-          fields: ['CATEGORIA', 'NUMERACAO', 'FORMATO_VENCIMENTO', 'AUDITORIA', 'CAMPOS_VENCIMENTO'],
-        });
-  
-        if (store) {
-          res.status(200).json({ success: true, message: 'Categoria criada.' });
-        } else {
-          console.log('Falha ao criar a categoria.');
-          res.status(400).json({ success: false, message: 'Falha ao criar a categoria.' });
-        }
-      } catch (error) {
-        if (error instanceof Sequelize.DatabaseError && error.name === 'SequelizeDatabaseError') {
-          // Erro de violação de restrição única
-          if (error.parent && error.parent.code === 'ORA-00001') {
-            console.error('Categoria já existe:', error);
-            res.status(400).json({ success: false, message: 'Categoria já existe.' });
-          }
-        } else if (error.name === 'TokenExpiredError') {
-          console.error('Token expirado:', error);
-          res.status(401).json({ success: false, message: 'Token expirado' });
-        } else {
-          console.error('Erro ao contatar o servidor:', error);
-          res.status(500).json({ success: false, message: 'Erro ao contatar o servidor' });
-        }
-      } finally {
-       
-      }
-    } else {
-      res.status(405).json({ success: false, message: 'Método não permitido.' });
+  if (req.method === 'POST') {
+    const { categoria, numeracao, formato_vencimento, auditoria, campos_vencimento, token } = req.body;
+
+    if (!token) {
+      return res.redirect(302, '/login');
     }
+
+    if (campos_vencimento === null || campos_vencimento === undefined) {
+      campos_vencimento = 'Não';
+    }
+
+    try {
+      jwt.verify(token, process.env.SECRET);
+
+      const store = await categoria_documentos.create({
+        CATEGORIA: categoria,
+        NUMERACAO: numeracao,
+        FORMATO_VENCIMENTO: formato_vencimento,
+        AUDITORIA: auditoria,
+        CAMPOS_VENCIMENTO: campos_vencimento,
+      }, {
+        fields: ['CATEGORIA', 'NUMERACAO', 'FORMATO_VENCIMENTO', 'AUDITORIA', 'CAMPOS_VENCIMENTO'],
+      });
+
+      if (store) {
+        res.status(200).json({ success: true, message: 'Categoria criada.' });
+      } else {
+        console.log('Falha ao criar a categoria.');
+        res.status(400).json({ success: false, message: 'Falha ao criar a categoria.' });
+      }
+    } catch (error) {
+      if (error instanceof Sequelize.DatabaseError && error.name === 'SequelizeDatabaseError') {
+        // Erro de violação de restrição única
+        if (error.parent && error.parent.code === 'ORA-00001') {
+          console.error('Categoria já existe:', error);
+          res.status(400).json({ success: false, message: 'Categoria já existe.' });
+        }
+      } else if (error.name === 'TokenExpiredError') {
+        console.error('Token expirado:', error);
+        res.status(401).json({ success: false, message: 'Token expirado' });
+      } else {
+        console.error('Erro ao contatar o servidor:', error);
+        res.status(500).json({ success: false, message: 'Erro ao contatar o servidor' });
+      }
+    } finally {
+
+    }
+  } else {
+    res.status(405).json({ success: false, message: 'Método não permitido.' });
   }
+}
