b796ba7ffe0a1f562b1cbad1dbcd8dd65960b200
AlessandroGeras <alessandrogeras@gmail.com>
2024-03-04 10:23:22 -0300

    Collaborator

--- pages/collaborators.tsx
+++ pages/collaborators.tsx
@@ -891,8 +891,8 @@ const Collaborators = () => {
               )}
 
               {documents.docs.rows.map((document: any, index) => (
-                /* Tamanho total tabela registros */
-                <div className='w-[930px]' key={document.id || index}>
+                /* Tamanho total tabela registros 1440*/
+                <div className='w-[1440px]' key={document.id || index}>
                   <div
                     className={`flex text-gray-700 whitespace-nowrap w-[3101px] overflow-x-auto  ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
                     key={document.id || Math.random().toString()}
