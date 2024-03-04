287d747aad4392645b2adfe690b0ab2023791bc1
AlessandroGeras <alessandrogeras@gmail.com>
2024-03-04 16:41:58 -0300

    Vigência

--- pages/api/find-status-outsourced.js
+++ pages/api/find-status-outsourced.js
@@ -6,62 +6,34 @@ import jwt from 'jsonwebtoken';
 dotenv.config();
 
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        
+  if (req.method === 'POST') {
+    const {
+      id,
+      token
+    } = req.body;
 
-        try {
-            jwt.verify(token, process.env.SECRET);
+    try {
+      jwt.verify(token, process.env.SECRET);  
 
-            const user = await outsourceds.findOne({
-                where: {
-                    NOME_TERCEIRO: id,
-                    COLABORADOR_TERCEIRO: 'N',
-                },
-            });
+      const user = await outsourceds.findOne({
+        where: {
+          NOME_TERCEIRO: id,
+          COLABORADOR_TERCEIRO: 'N',
 
+        },
+      });
+            
 
-            if (!user) {
-                return res.status(403).json({ error: 'Falha ao localizar o Terceiro.' });
-            }
+      if(!user){
+        return res.status(403).json({ error: 'Falha ao localizar o Terceiro.' });
+      }
 
-            res.status(200).json({ user });
-        } catch (error) {
-            console.error('Falha ao consultar o banco de dados:', error);
-            res.status(500).json({ error: 'Erro ao consultar o banco de dados:' + error });
-        } finally {
-
-        }
+      res.status(200).json({ user });
+    } catch (error) {
+      console.error('Falha ao consultar o banco de dados:', error);
+      res.status(500).json({ error: 'Erro ao consultar o banco de dados:' + error });
+    } finally {
+      
     }
-    else if (req.method === 'POST') {
-        const {
-            id,
-            selectedStatus,
-            token
-        } = req.body;
-
-
-        try {
-            const user = await outsourceds.findOne({
-                where: {
-                    NOME_TERCEIRO: id,
-                    COLABORADOR_TERCEIRO: 'N',
-                },
-            });
-
-            user.STATUS = selectedStatus;
-
-            await existingUser.save();
-
-            if (!user) {
-                return res.status(403).json({ error: 'Falha ao localizar o Terceiro.' });
-            }
-
-            res.status(200).json({ success: true, message: 'Status salvo.' });
-        } catch (error) {
-            console.error('Falha ao consultar o banco de dados:', error);
-            res.status(500).json({ error: 'Erro ao consultar o banco de dados:' + error });
-        } finally {
-
-        }
-    }
+  }
 }
---
+++ pages/api/update-status-outsourced.js
@@ -0,0 +1,53 @@
+import outsourceds from '../../models/outsourceds';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const {
+            id,
+            selectedStatus,
+            id_user,
+            cnpj,
+            usuario,
+            sobrenome,
+            endereco,
+            cidade,
+            email,
+            telefone,
+            uf,
+            principal,
+            status,
+            obs_status,
+            nome_terceiro,
+            token,
+        } = req.body;        
+
+        try {
+            jwt.verify(token, process.env.SECRET);  
+
+            const existingUser = await outsourceds.findOne({
+                where: {
+                  NOME_TERCEIRO: id,
+                  COLABORADOR_TERCEIRO: 'N',
+        
+                },
+              }); 
+
+            
+            existingUser.STATUS = selectedStatus;     
+
+            await existingUser.save();
+
+            res.status(200).json({ user });
+
+        } catch (error) {
+            console.error('Erro ao alterar os dados do Terceiro:', error);
+            res.status(500).json({ success: false, message: 'Erro ao alterar os dados do Terceiro.' });
+        }
+    } else {
+        res.status(405).json({ success: false, message: 'Método não permitido.' });
+    }
+}
