35618204cfaaa1ffe264811df7d983ddaa87618d
AlessandroGeras <alessandrogeras@gmail.com>
2024-03-22 09:37:26 -0300

    Outsourced

--- pages/find-document.tsx
+++ pages/find-document.tsx
@@ -559,7 +559,6 @@ const FindDocument = () => {
                         </div>
                         <div className="mt-6">
                             <label htmlFor="identificacao" className="block text-sm font-medium text-gray-700">
-                                <span className="text-red-500">*</span>
                             </label>
                             <input
                                 type="text"
