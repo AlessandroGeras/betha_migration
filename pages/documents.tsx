9e1162b47cfa5a69376a955df7c4d19f744b6fc3
AlessandroGeras <alessandrogeras@gmail.com>
2024-03-25 10:04:06 -0300

    Documents

--- pages/documents.tsx
+++ pages/documents.tsx
@@ -209,7 +209,7 @@ const Users = () => {
     'STATUS': '200px',
     'TIPO_DOCUMENTO': '520px',
     'TERCEIRO': '330px',
-    'COLABORADOR': '500px',
+    'COLABORADOR': '350px',
     'VENCIMENTO': '260px',
   };
 
@@ -1010,7 +1010,7 @@ const Users = () => {
           {/* LARGURA DA LISTA PRINCIPAL Fixa 1440px*/}
             <div className="flex flex-col h-[550px] w-[1440px] overflow-y-auto overflow-x-auto text-ellipsis overflow-hidden">
               {/* LARGURA DOS FILTROS DA LISTA */}
-              <div className="flex text-gray-500 bg-white w-[1850px]">
+              <div className="flex text-gray-500 bg-white w-[1700px]">
                 {Object.keys(columnWidths).map((column) => (
                   <div
                     key={column}
@@ -1041,7 +1041,7 @@ const Users = () => {
 
             {/* LARGURA DOS CAMPOS DE FILTRO INTERNOS */} 
               {filterOpen && (
-                <div className={`flex text-gray-500 w-[1850px]`}>
+                <div className={`flex text-gray-500 w-[1700px]`}>
                   <div className={`header-cell border border-gray-300 py-1 pl-1 cursor-pointer flex`} style={{ width: '69px' }}>
                     <div className="flex items-center">
                     </div>
@@ -1169,9 +1169,9 @@ const Users = () => {
 
             {/* LARGURA DO CONTEÚDO INTERNO DA LISTA PRINCIPAL */}
               {documents.docs.rows.map((document, index) => (
-                <div className='w-[1850px]' key={document.id || Math.random().toString()}>
+                <div className='w-[1700px]' key={document.id || Math.random().toString()}>
                   <div
-                    className={`flex text-gray-700 whitespace-nowrap w-[1850px] text-ellipsis overflow-hidden  ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
+                    className={`flex text-gray-700 whitespace-nowrap w-[1700px] text-ellipsis overflow-hidden  ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}`}
                   >
                     {Object.keys(columnWidths).map((column) => (
                       <div
